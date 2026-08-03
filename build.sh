4b228fa23f29b1dbc43f9b4e2ebbcf0cad533f57
lusid1 <lusid@lusid.net>
2026-08-03 15:00:01 -0700

    add test case 2

--- build.sh
+++ build.sh
@@ -41,8 +41,33 @@ wget "$(cat $dlc_db | grep $dlc2 | cut -f 4)" -O $dlcpkg2
 pkg2zip/pkg2zip -d -x $dlcpkg1 # decrypted
 pkg2zip/pkg2zip -p -x $dlcpkg2 # encrypted
 
+# Test Case 2 : LittleBigPlanet
+cid="UP9000-UCUS98744_00-LBPPSPAFULL00001"
+pkg="pkg/$cid.pkg"
+echo "fetching $(cat $games_db | grep $cid | cut -f 4)"
+wget "$(cat $games_db | grep $cid | cut -f 5)" -O $pkg
+# save the iso and the pbp
+pkg2zip/pkg2zip -x $pkg
+pkg2zip/pkg2zip -p -x $pkg
 
+# LBP DLC requires a patch pboot:
+cid="UP9000-UCUS98744_00-LBPPDLCSONYPA002"
+pkg="pkg/$cid.pkg"
+echo "fetching $(cat $updates_db | grep $cid | cut -f 3)"
+wget "$(cat $updates_db | grep $cid | cut -f 4)" -O $pkg
+pkg2zip/pkg2zip -p -x $pkg
 
+# now we need 2 DLC 
+dlc1="UP9000-UCUS98744_00-LBPPDLCSONYLP007" # Easter Pack
+dlc2="UP9000-UCUS98744_00-LBPPDLCSONYLP006" # Yuletide Sack
+dlcpkg1="pkg/$dlc1.pkg"
+dlcpkg2="pkg/$dlc2.pkg"
+echo "fetching $(cat $dlc_db | grep $dlc1 | cut -f 3)"
+wget "$(cat $dlc_db | grep $dlc1 | cut -f 4)" -O $dlcpkg1
+echo "fetching $(cat $dlc_db | grep $dlc2 | cut -f 3)"
+wget "$(cat $dlc_db | grep $dlc2 | cut -f 4)" -O $dlcpkg2
+pkg2zip/pkg2zip -d -x $dlcpkg1 # decrypted
+pkg2zip/pkg2zip -p -x $dlcpkg2 # encrypted
 
 
 
